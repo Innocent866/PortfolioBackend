9d7af0ad1af9d906e05cc1079635161585a78570
Innocent866 <igoldima@gmail.com>
2024-02-07 20:49:50 -0800

    Portfolio

--- Controller/userController.js
+++ Controller/userController.js
@@ -2,8 +2,14 @@ const USER = require ('../Model/user')
 
 // create user
 const create_user = async (req,res) =>{
+    const {name,email,password} = req.body
+
+    if (!name || !email || !password ) {
+        res.status(300).json({success:false,message:"all fields are require to register"})
+        return
+    }
     try {
-        await USER.create({...req.body})
+        await USER.create(req.body)
         res.status(201).json({success:true,message:'Registration Successful'})
     } catch (error) {
         console.log();
